b8a39aa1f522faa6a434a73b11da30472a7cbf82
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-01-21 16:03:34 +0500

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -39,3 +39,107 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// import './App.css';
+// import io from 'socket.io-client';
+// import { useEffect, useState, useRef } from 'react';
+
+// const socket = io.connect('http://localhost:3001');
+// // ... (previous imports)
+
+// // ... (previous imports)
+
+// // ... (previous imports)
+
+// function App() {
+//   const [room, setRoom] = useState('');
+//   const [message, setMessage] = useState('');
+//   const [messageReceived, setMessageReceived] = useState('');
+//   const [localStream, setLocalStream] = useState(null);
+//   const [remoteStreams, setRemoteStreams] = useState([]);
+//   const [isCalling, setIsCalling] = useState(false);
+//   const [callerId, setCallerId] = useState('');
+//   const videoRef = useRef();
+
+//   const startCall = (user) => {
+//     socket.emit('call-user', { roomId: room, targetUserId: user.id, callerStream: localStream });
+//   };
+
+//   const answerCall = () => {
+//     setIsCalling(false);
+//     socket.emit('answer-call', { roomId: room, targetUserId: callerId, callerStream: localStream });
+//   };
+
+//   const endCall = (user) => {
+//     setIsCalling(false);
+//     setCallerId('');
+//     socket.emit('end-call', { roomId: room, targetUserId: user.id });
+//   };
+
+//   // ... (other state and useEffect)
+
+//   useEffect(() => {
+//     // ... (previous code)
+
+//     socket.on('receive-call', (incomingStream) => {
+//       setIsCalling(true);
+//       setCallerId(incomingStream.id);
+//     });
+
+//     socket.on('answer-call', () => {
+//       setIsCalling(false);
+//     });
+
+//     socket.on('call-ended', () => {
+//       setIsCalling(false);
+//       setCallerId('');
+//     });
+
+//     return () => {
+//       socket.off('receive-call');
+//       socket.off('answer-call');
+//       socket.off('call-ended');
+//     };
+//   }, []);
+
+//   // ... (other functions)
+
+//   return (
+//     <div className="App">
+//       {/* ... (previous JSX code) */}
+//       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+//         <video ref={videoRef} autoPlay muted style={{ width: '300px', height: '200px' }} />
+
+//         {remoteStreams.map((remoteStream) => (
+//           <div key={remoteStream.id} style={{ position: 'relative', margin: '10px' }}>
+//             <video ref={(ref) => (ref.srcObject = remoteStream)} autoPlay style={{ width: '300px', height: '200px' }} />
+
+//             {!isCalling && (
+//               <button style={{ position: 'absolute', bottom: '0', right: '0' }} onClick={() => startCall(remoteStream)}>
+//                 Call
+//               </button>
+//             )}
+
+//             {isCalling && remoteStream.id === callerId && (
+//               <button style={{ position: 'absolute', bottom: '0', right: '0' }} onClick={answerCall}>
+//                 Answer
+//               </button>
+//             )}
+
+//             <button style={{ position: 'absolute', top: '0', left: '0' }} onClick={() => endCall(remoteStream)}>
+//               End Call
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
--- server/index.js
+++ server/index.js
@@ -32,3 +32,59 @@ io.on("connection", (socket) => {
 server.listen(3001, () => {
     console.log("server is running")
 })
+
+
+// server.js
+
+// const express = require("express");
+// const http = require("http");
+// const socketIO = require("socket.io");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIO(server);
+
+// const rooms = {};
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
+
+//   socket.on("join_room", ({ room, signalData }) => {
+//     if (!rooms[room]) {
+//       rooms[room] = [socket.id];
+//     } else if (rooms[room].length === 1) {
+//       rooms[room].push(socket.id);
+//       const otherUser = rooms[room][0];
+//       io.to(otherUser).emit("receive_signal", { signalData, callerID: socket.id });
+//     } else {
+//       // Room is full
+//       socket.emit("room_full");
+//     }
+
+//     socket.join(room);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     io.to(data.room).emit("receive_message", { message: data.message });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected");
+//     for (const room in rooms) {
+//       const index = rooms[room].indexOf(socket.id);
+//       if (index !== -1) {
+//         rooms[room].splice(index, 1);
+//         if (rooms[room].length === 0) {
+//           delete rooms[room];
+//         }
+//         break;
+//       }
+//     }
+//   });
+// });
+
+// const PORT = process.env.PORT || 3001;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
